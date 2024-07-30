b55dbe6ba20f7b4214fb8e66e755f6158ac64db3
dope0515 <61546301+dope0515@users.noreply.github.com>
2024-07-30 16:58:18 +0900

    메인페이지 작업 완료

---
+++ include/arr_data.php
@@ -0,0 +1,87 @@
+<?
+
+$site_title = ""; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "ABOUT US";
+$nav_1_1 = "기업소개";
+$nav_1_2 = "CEO 인사말";
+$nav_1_3 = "연혁";
+$nav_1_4 = "사업장";
+
+$nav_2 = "제품소개";
+$nav_2_1 = "Semicon";
+$nav_2_2 = "FR-TP";
+$nav_2_3 = "FR-XLPE";
+$nav_2_4 = "Rubber";
+
+$nav_3 = "기술연구소";
+$nav_3_1 = "Our vision";
+$nav_3_2 = "Our portfolio";
+$nav_3_3 = "Patent";
+
+$nav_3 = "기술연구소";
+$nav_3_1 = "Our vision";
+$nav_3_2 = "Our portfolio";
+$nav_3_3 = "Patent";
+
+$nav_4 = "지속가능경영";
+$nav_4_1 = "안전환경경영";
+$nav_4_2 = "윤리경영";
+$nav_4_3 = "품질경영";
+$nav_4_4 = "사회공헌";
+
+$nav_5 = "인재채용";
+$nav_5_1 = "가치";
+$nav_5_2 = "채용정보";
+$nav_5_3 = "채용공고";
+
+$nav_6 = "뉴스 & 소식";
+$nav_6_1 = "뉴스";
+$nav_6_2 = "소식";
+
+$nav_7 = "문의하기";
+$nav_7_1 = "문의하기";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../about/about-us.html";
+$nav_1_1_href = "../about/about-us.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
